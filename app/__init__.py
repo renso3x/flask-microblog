cf907dbddd1dd003ee02e90cb93f3499e59b4588
renso3x <enso123456@gmail.com>
2020-09-04 17:16:06 +0800

    login and register using flasklogin and werkzeug

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,5 +11,8 @@ app.config.from_object(Config)
 # SQL Alchemy
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = "login"
 
 from app import routes, models
