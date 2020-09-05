9d61d96109bc4ecf63a1a2c5e9a655a6ef67b790
renso3x <enso123456@gmail.com>
2020-09-05 13:31:52 +0800

    add edit profile

--- app/models.py
+++ app/models.py
@@ -12,6 +12,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         return "<User {}>".format(self.username)
 
